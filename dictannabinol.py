85a7251cb6f00bb3fad357f149a44ad1afeda936
pivje <pivertjerome@gmail.com>
2016-04-15 17:26:58 +0200

    forgot is now a function

--- dictannabinol.py
+++ dictannabinol.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-import random as rand
+import random
 
 class Dictannabinol(dict):
     """
         Dictionnary that forgets things based on a trust ratio
-        This trust ratio is the chance of forgetting (in percentage)
+        This trust ratio is the viability of the object (in percentage)
+
+        All overrided functions have a (small) chance to loose some data
+        the chance to remove the data is 1 minus threshold thus it
+        is said here it wont be in each function comments
+
+
+        A trust = 1 is correspond to an object that can only loose
     """
 
-    # Constructors
-    def __init__(self, trust_percent = 0.25):
+
+    #######################
+    # Dictannabinol basis #
+    #######################
+    def __init__(self, trust_percent = 0.75):
         """
             Default constructor for a Dictannabinol
             
-            _trust percentage of reliability of a dictannabinol (default: 0.25)
+            _trust percentage of reliability of a dictannabinol (default: 0.75)
         """
         super().__init__(self)
 
@@ -22,7 +32,6 @@ class Dictannabinol(dict):
             self._trust = trust_percent # _trust is protected (convention)
         else:
             raise ValueError('trust ratio must be within 0-1')
-
 
 
     @property
@@ -41,36 +50,70 @@ class Dictannabinol(dict):
             raise ValueError('trust ratio must be within 0-1')
 
 
-    # Override dict functions
+
+
+    ##################
+    # Misc functions #
+    ##################
+    def __forgot(self, threshold, key=False):
+        """
+            Forgot something based on a treshold, if above treshold -> removed
+            if no key specified the removal will be random
+
+            threshold from 0 to 1, chance to not be removed
+
+            If the object is trusty, you wont forget
+        """
+        # If trusty, stay trusty and don't forget
+        if self._trust < 1:
+            n = random.random()
+
+            if key and key in super().keys() and n > threshold:
+                super().__delitem__(key)
+            elif n > threshold:
+                key = random.choice(list(super().keys()))
+                super().__delitem__(key)
+            else:
+                pass
+
+
+
+
+    ###########################
+    # Override dict functions #
+    ###########################
     def __contains__(self, query):
         """ Give unreliable informations about the existence of keys in dict """
-        if rand.random() > self._trust:
+
+        # 1 - % chance of random removal
+        self.__forgot(threshold = 0.98, key = query)
+
+        # actual method
+        if random.random() > self._trust:
             return False
         else:
             return super().__contains__(query)
 
     def __setitem__(self, key, value):
         """
-            Add (or not) the key:value to the dictannabinol
-            The threshold is trust * 3
+            Add (or not) the key:value to the dictannabinol based
+            on a trust value
 
-            if trust > 0.333.. the __setitem__ is reliable
+            N.B. Here we use the __forgot function to simulate the "not adding
+            the value"
         """
-        treshold = self._trust * 3
-        if rand.random() <= treshold:
-            return super().__setitem__(key, value)
-        else:
-            pass
+        super().__setitem__(key, value)
+        self.__forgot(threshold = self._trust, key = key)
 
     def __getitem__(self, key):
         """
-            Get value from key (or not)
-            The threshold is trust * 1.5
-
-            if trust > 0.666.. the __getitem__ is reliable
+            Get value from key (or not) based on the trust value
         """
-        treshold = self._trust * 1.5
-        if rand.random() <= treshold:
+        # 1 - % chance of random removal
+        self.__forgot(threshold = 0.95)
+
+        # actual method
+        if random.random() <= self._trust:
             return super().__getitem__(key)
         else:
             raise KeyError(key)
